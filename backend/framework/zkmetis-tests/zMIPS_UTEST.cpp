d7db632afcb051df37e92e27f641eb59780ff2ef
Shailesh B Nair <shai567@live.com>
2022-10-06 15:50:46 +0530

    Updated UTEST file

--- backend/framework/zkmetis-tests/zMIPS_UTEST.cpp
+++ backend/framework/zkmetis-tests/zMIPS_UTEST.cpp
@@ -1,16 +1,16 @@
 #include <gtest/gtest.h>
 #include "executeProtocol.hpp"
 #include "argParser.hpp"
-#include "zMipsParser.hpp"
+#include "zkMipsParser.hpp"
 #include "RAMtoBair/RamToContraintSystem/ALU.hpp"
 
 
 int securityParameter = 60;
-const string macros_file = "./framework/zilch/src/macros.json";
+const string macros_file = "./framework/zmetis/src/macros.json";
 
 TEST(zMIPS, factorial) {
-	string assembly_file = "./examples-zmips/factorial/fact.zmips";
-	string public_tape = "./examples-zmips/factorial/fact2.pubtape";
+	string assembly_file = "./examples-zkmips/factorial/fact.zmips";
+	string public_tape = "./examples-zkmips/factorial/fact2.pubtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -18,8 +18,8 @@ TEST(zMIPS, factorial) {
 }
 
 TEST(zMIPS, fibonacci) {
-	string assembly_file = "./examples-zmips/fibonacci/fib.zmips";
-	string public_tape = "./examples-zmips/fibonacci/fib2.pubtape";
+	string assembly_file = "./examples-zkmips/fibonacci/fib.zmips";
+	string public_tape = "./examples-zkmips/fibonacci/fib2.pubtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -27,9 +27,9 @@ TEST(zMIPS, fibonacci) {
 }
 
 TEST(zMIPS, isort) {
-	string assembly_file = "./examples-zmips/isort/isort.zmips";
-	string public_tape = "./examples-zmips/isort/isort.pubtape";
-	string private_tape = "./examples-zmips/isort/isort.auxtape";
+	string assembly_file = "./examples-zkmips/isort/isort.zmips";
+	string public_tape = "./examples-zkmips/isort/isort.pubtape";
+	string private_tape = "./examples-zkmips/isort/isort.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -37,9 +37,9 @@ TEST(zMIPS, isort) {
 }
 
 TEST(zMIPS, knowledge_of_factorization) {
-	string assembly_file = "./examples-zmips/knowledge_of_factorization/knowledge_of_factorization.zmips";
-	string public_tape = "./examples-zmips/knowledge_of_factorization/knowledge_of_factorization.pubtape";
-	string private_tape = "./examples-zmips/knowledge_of_factorization/knowledge_of_factorization.auxtape";
+	string assembly_file = "./examples-zkmips/knowledge_of_factorization/knowledge_of_factorization.zmips";
+	string public_tape = "./examples-zkmips/knowledge_of_factorization/knowledge_of_factorization.pubtape";
+	string private_tape = "./examples-zkmips/knowledge_of_factorization/knowledge_of_factorization.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -47,9 +47,9 @@ TEST(zMIPS, knowledge_of_factorization) {
 }
 
 TEST(zMIPS, knowledge_of_RSA_private_key) {
-	string assembly_file = "./examples-zmips/knowledge_of_RSA_private_key/knowledge_of_RSA_private_key.zmips";
-	string public_tape = "./examples-zmips/knowledge_of_RSA_private_key/knowledge_of_RSA_private_key.pubtape";
-	string private_tape = "./examples-zmips/knowledge_of_RSA_private_key/knowledge_of_RSA_private_key.auxtape";
+	string assembly_file = "./examples-zkmips/knowledge_of_RSA_private_key/knowledge_of_RSA_private_key.zmips";
+	string public_tape = "./examples-zkmips/knowledge_of_RSA_private_key/knowledge_of_RSA_private_key.pubtape";
+	string private_tape = "./examples-zkmips/knowledge_of_RSA_private_key/knowledge_of_RSA_private_key.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -57,8 +57,8 @@ TEST(zMIPS, knowledge_of_RSA_private_key) {
 }
 
 TEST(zMIPS, mmult) {
-	string assembly_file = "./examples-zmips/mmult/mmult.zmips";
-	string private_tape = "./examples-zmips/mmult/mmult.auxtape";
+	string assembly_file = "./examples-zkmips/mmult/mmult.zmips";
+	string private_tape = "./examples-zkmips/mmult/mmult.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -66,9 +66,9 @@ TEST(zMIPS, mmult) {
 }
 
 TEST(zMIPS, pir) {
-	string assembly_file = "./examples-zmips/pir/pir.zmips";
-	string public_tape = "./examples-zmips/pir/pir2.pubtape";
-	string private_tape = "./examples-zmips/pir/pir.auxtape";
+	string assembly_file = "./examples-zkmips/pir/pir.zmips";
+	string public_tape = "./examples-zkmips/pir/pir2.pubtape";
+	string private_tape = "./examples-zkmips/pir/pir.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -76,9 +76,9 @@ TEST(zMIPS, pir) {
 }
 
 TEST(zMIPS, range_query) {
-	string assembly_file = "./examples-zmips/range_query/range_query.zmips";
-	string public_tape = "./examples-zmips/range_query/range_query2.pubtape";
-	string private_tape = "./examples-zmips/range_query/range_query.auxtape";
+	string assembly_file = "./examples-zkmips/range_query/range_query.zmips";
+	string public_tape = "./examples-zkmips/range_query/range_query2.pubtape";
+	string private_tape = "./examples-zkmips/range_query/range_query.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -86,9 +86,9 @@ TEST(zMIPS, range_query) {
 }
 
 TEST(zMIPS, read_test) {
-	string assembly_file = "./examples-zmips/read_test/read_test.zmips";
-	string public_tape = "./examples-zmips/read_test/read_test.pubtape";
-	string private_tape = "./examples-zmips/read_test/read_test.auxtape";
+	string assembly_file = "./examples-zkmips/read_test/read_test.zmips";
+	string public_tape = "./examples-zkmips/read_test/read_test.pubtape";
+	string private_tape = "./examples-zkmips/read_test/read_test.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, public_tape, macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -98,8 +98,8 @@ TEST(zMIPS, read_test) {
 #if REGISTER_LENGTH == 16
 
 TEST(zMIPS, simon_32_64) {
-	string assembly_file = "./examples-zmips/simon/simon32.zmips";
-	string private_tape = "./examples-zmips/simon/simon32.auxtape";
+	string assembly_file = "./examples-zkmips/simon/simon32.zmips";
+	string private_tape = "./examples-zkmips/simon/simon32.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -107,8 +107,8 @@ TEST(zMIPS, simon_32_64) {
 }
 
 TEST(zMIPS, speck_32_64) {
-	string assembly_file = "./examples-zmips/speck/speck32.zmips";
-	string private_tape = "./examples-zmips/speck/speck32.auxtape";
+	string assembly_file = "./examples-zkmips/speck/speck32.zmips";
+	string private_tape = "./examples-zkmips/speck/speck32.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -116,8 +116,8 @@ TEST(zMIPS, speck_32_64) {
 }
 
 TEST(zMIPS, simon_32_64_hash) {
-	string assembly_file = "./examples-zmips/simon_DM_hash/simon32_DM_hash.zmips";
-	string private_tape = "./examples-zmips/simon_DM_hash/simon32_DM_hash.auxtape";
+	string assembly_file = "./examples-zkmips/simon_DM_hash/simon32_DM_hash.zmips";
+	string private_tape = "./examples-zkmips/simon_DM_hash/simon32_DM_hash.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -125,8 +125,8 @@ TEST(zMIPS, simon_32_64_hash) {
 }
 
 TEST(zMIPS, speck_32_64_hash) {
-	string assembly_file = "./examples-zmips/speck_DM_hash/speck32_DM_hash.zmips";
-	string private_tape = "./examples-zmips/speck_DM_hash/speck32_DM_hash.auxtape";
+	string assembly_file = "./examples-zkmips/speck_DM_hash/speck32_DM_hash.zmips";
+	string private_tape = "./examples-zkmips/speck_DM_hash/speck32_DM_hash.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -136,8 +136,8 @@ TEST(zMIPS, speck_32_64_hash) {
 #elif REGISTER_LENGTH == 32
 
 TEST(zMIPS, simon_64_128) {
-	string assembly_file = "./examples-zmips/simon/simon64.zmips";
-	string private_tape = "./examples-zmips/simon/simon64.auxtape";
+	string assembly_file = "./examples-zkmips/simon/simon64.zmips";
+	string private_tape = "./examples-zkmips/simon/simon64.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -145,8 +145,8 @@ TEST(zMIPS, simon_64_128) {
 }
 
 TEST(zMIPS, speck_64_128) {
-	string assembly_file = "./examples-zmips/speck/speck64.zmips";
-	string private_tape = "./examples-zmips/speck/speck64.auxtape";
+	string assembly_file = "./examples-zkmips/speck/speck64.zmips";
+	string private_tape = "./examples-zkmips/speck/speck64.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -154,8 +154,8 @@ TEST(zMIPS, speck_64_128) {
 }
 
 TEST(zMIPS, simon_64_128_hash) {
-	string assembly_file = "./examples-zmips/simon_DM_hash/simon64_DM_hash.zmips";
-	string private_tape = "./examples-zmips/simon_DM_hash/simon64_DM_hash.auxtape";
+	string assembly_file = "./examples-zkmips/simon_DM_hash/simon64_DM_hash.zmips";
+	string private_tape = "./examples-zkmips/simon_DM_hash/simon64_DM_hash.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -163,8 +163,8 @@ TEST(zMIPS, simon_64_128_hash) {
 }
 
 TEST(zMIPS, speck_64_128_hash) {
-	string assembly_file = "./examples-zmips/speck_DM_hash/speck64_DM_hash.zmips";
-	string private_tape = "./examples-zmips/speck_DM_hash/speck64_DM_hash.auxtape";
+	string assembly_file = "./examples-zkmips/speck_DM_hash/speck64_DM_hash.zmips";
+	string private_tape = "./examples-zkmips/speck_DM_hash/speck64_DM_hash.auxtape";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, private_tape, 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -175,7 +175,7 @@ TEST(zMIPS, speck_64_128_hash) {
 
 
 TEST(zMIPS, lw_sw) {
-	string assembly_file = "./examples-zmips/lw_sw.zmips";
+	string assembly_file = "./examples-zkmips/lw_sw.zmips";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -183,7 +183,7 @@ TEST(zMIPS, lw_sw) {
 }
 
 TEST(zMIPS, min_test) {
-	string assembly_file = "./examples-zmips/min_test.zmips";
+	string assembly_file = "./examples-zkmips/min_test.zmips";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -191,7 +191,7 @@ TEST(zMIPS, min_test) {
 }
 
 TEST(zMIPS, collatz) {
-	string assembly_file = "./examples-zmips/collatz.zmips";
+	string assembly_file = "./examples-zkmips/collatz.zmips";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -199,7 +199,7 @@ TEST(zMIPS, collatz) {
 }
 
 TEST(zMIPS, simple_add) {
-	string assembly_file = "./examples-zmips/simple_add.zmips";
+	string assembly_file = "./examples-zkmips/simple_add.zmips";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -207,7 +207,7 @@ TEST(zMIPS, simple_add) {
 }
 
 TEST(zMIPS, simple_loop) {
-	string assembly_file = "./examples-zmips/simple_loop.zmips";
+	string assembly_file = "./examples-zkmips/simple_loop.zmips";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
@@ -215,7 +215,7 @@ TEST(zMIPS, simple_loop) {
 }
 
 TEST(zMIPS, swap_test) {
-	string assembly_file = "./examples-zmips/swap_test.zmips";
+	string assembly_file = "./examples-zkmips/swap_test.zmips";
 	string asm_parsed = parse_zmips(assembly_file, "", macros_file, false);
 	execute_locally(asm_parsed, "", 0, securityParameter, false, true, false);
 	std::remove(asm_parsed.c_str());
