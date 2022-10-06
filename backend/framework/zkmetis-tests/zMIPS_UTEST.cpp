ad1abe370b2a2440722fcc6f62026ebd76b211ca
Shailesh B Nair <shai567@live.com>
2022-10-06 15:57:19 +0530

    Updated UTEST files

--- backend/framework/zkmetis-tests/zMIPS_UTEST.cpp
+++ backend/framework/zkmetis-tests/zMIPS_UTEST.cpp
@@ -6,7 +6,7 @@
 
 
 int securityParameter = 60;
-const string macros_file = "./framework/zmetis/src/macros.json";
+const string macros_file = "./framework/zkmetis/src/macros.json";
 
 TEST(zMIPS, factorial) {
 	string assembly_file = "./examples-zkmips/factorial/fact.zmips";
